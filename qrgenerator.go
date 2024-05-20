9d2f4a2b30c84d83c71e48d5468454566bef20c6
Kornsakon <87508519+kjpie@users.noreply.github.com>
2024-05-20 12:52:39 +0700

    Update qrgenerator.go

--- qrgenerator.go
+++ qrgenerator.go
@@ -13,18 +13,20 @@ import (
 	"os"
 )
 
-func QRForTargetWithAmount(target, amount string) (*[]byte, error) {
+func QRForTargetWithAmount(target, amount string) (string, error) {
 
 	qr := New()
 	payload := qr.GeneratePayload(target, &amount)
 
-	var png []byte
-	png, err := qrcode.Encode(payload, qrcode.Medium, 256)
+	// fmt.Println("payload", payload)
 
-	if err != nil {
-		return nil, err
-	}
-	return &png, nil
+	// var png []byte
+	// png, err := qrcode.Encode(payload, qrcode.Medium, 256)
+
+	// if err != nil {
+	// 	return nil, err
+	// }
+	return payload, nil
 }
 
 func QRForBillPayment(billerID string, ref1 string, ref2 string, terminalID string, amount string) (*[]byte, error) {
